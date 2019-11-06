d00a96ce9c08284c23eb32de006cde4f3d074c4f
Siqi Ren <siqi.ren@gmail.com>
2019-11-05 21:24:07 -0500

    created update function

--- 6. Using Data From Firebase/index.js
+++ 6. Using Data From Firebase/index.js
@@ -11,13 +11,83 @@ const graphWidth = 600 - margin.left - margin.right;
 const graphHeight = 600 - margin.top - margin.bottom;
 
 const graph = svg
-  .append('g')
+  .append('g') // create group
   .attr('width', graphWidth)
   .attr('height', graphHeight)
   .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 const xAxisGroup = graph.append('g').attr('transform', `translate(0, ${graphHeight})`);
 const yAxisGroup = graph.append('g');
+
+// init set linear scale
+const y = d3.scaleLinear().range([graphHeight, 0]);
+
+// init set band scale
+const x = d3
+  .scaleBand()
+  .range([0, 500])
+  .paddingInner(0.2)
+  .paddingOuter(0.2);
+
+// create the axes
+const xAxis = d3.axisBottom(x);
+const yAxis = d3
+  .axisLeft(y)
+  .ticks(3) // try to get only around 3 ticks
+  .tickFormat(d => d + '  orders');
+
+// apply transform/rotate on all text on x axis
+xAxisGroup
+  .selectAll('text')
+  .attr('fill', 'orange')
+  .attr('transform', 'rotate(-40)')
+  .attr('text-anchor', 'end'); // rotate at the end of the text
+
+// const min = d3.min(data, d => d.orders);
+// const max = d3.max(data, d => d.orders);
+// const extent = d3.extent(data, d => d.orders); // find both min and max
+
+// update function
+const update = data => {
+  /** 1. update any scale (domains) if they rely on our data */
+
+  // update linear scale
+  y.domain([0, d3.max(data, d => d.orders)]); // min 0, max 1000
+
+  // update band scale
+  x.domain(data.map(item => item.name));
+
+  /** 2. join updated data to elements */
+  // join data to rects
+  const rects = graph.selectAll('rect').data(data);
+
+  /** 3. remove unwanted shapes using the exit selection */
+  rects.exit().remove();
+
+  /** 4. update current shapes in the dom */
+  // add attrs to rects already in DOM, just in case there's data already in DOM
+  rects
+    .attr('width', x.bandwidth())
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders)); // rect always runs from top to down, so we need to set a starting/top position
+
+  /** 5. append the enter selection to the dom */
+  // append new rects
+  rects
+    .enter() // create new rect thats not in the DOM
+    .append('rect')
+    .attr('width', x.bandwidth())
+    .attr('height', d => graphHeight - y(d.orders))
+    .attr('fill', 'orange')
+    .attr('x', d => x(d.name))
+    .attr('y', d => y(d.orders));
+
+  // call the axes
+  xAxisGroup.call(xAxis);
+  yAxisGroup.call(yAxis);
+};
 
 db.collection('dishes')
   .get()
@@ -27,59 +97,5 @@ db.collection('dishes')
       data.push(doc.data());
     });
 
-    const min = d3.min(data, d => d.orders);
-    const max = d3.max(data, d => d.orders);
-    const extent = d3.extent(data, d => d.orders); // find both min and max
-
-    // linear scale
-    const y = d3
-      .scaleLinear()
-      .domain([0, max]) // min 0, max 1000
-      .range([graphHeight, 0]);
-
-    // band scale
-    const x = d3
-      .scaleBand()
-      .domain(data.map(item => item.name))
-      .range([0, 500])
-      .paddingInner(0.2)
-      .paddingOuter(0.2);
-
-    // join data to rects
-    const rects = graph.selectAll('rect').data(data);
-
-    // add attrs to rects already in DOM, just in case there's data already in DOM
-    rects
-      .attr('width', x.bandwidth())
-      .attr('height', d => graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', d => x(d.name))
-      .attr('y', d => y(d.orders)); // rect always runs from top to down, so we need to set a starting/top position
-
-    // append new rects
-    rects
-      .enter()
-      .append('rect')
-      .attr('width', x.bandwidth())
-      .attr('height', d => graphHeight - y(d.orders))
-      .attr('fill', 'orange')
-      .attr('x', d => x(d.name))
-      .attr('y', d => y(d.orders));
-
-    // create and call the
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3
-      .axisLeft(y)
-      .ticks(3) // try to get only around 3 ticks
-      .tickFormat(d => d + '  orders');
-
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
-
-    // apply transform/rotate on all text on x axis
-    xAxisGroup
-      .selectAll('text')
-      .attr('fill', 'orange')
-      .attr('transform', 'rotate(-40)')
-      .attr('text-anchor', 'end'); // rotate at the end of the text
+    update(data);
   });
