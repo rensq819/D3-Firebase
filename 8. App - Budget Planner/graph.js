ff65b92b8891834c9b96810192f89f31218982c9
Siqi Ren <siqi.ren@gmail.com>
2019-11-16 21:57:53 -0500

    added path for donut chart

--- 8. App - Budget Planner/graph.js
+++ 8. App - Budget Planner/graph.js
@@ -15,9 +15,47 @@ const pie = d3
   .sort(null) // not sort based on value, which might be default by d3
   .value(d => d.cost);
 
-const angles = pie([{ name: 'rent', cost: 500 }, { name: 'bills', cost: 300 }, { name: 'gaming', cost: 200 }]);
-
 const arcPath = d3
   .arc()
   .outerRadius(dims.radius)
   .innerRadius(dims.radius / 2); // for donut chart
+
+// update function
+const update = data => {
+  // join enhanced (pie)data to path elements
+  const paths = graph.selectAll('path').data(pie(data));
+
+  paths
+    .enter()
+    .append('path')
+    .attr('class', 'arc')
+    .attr('d', arcPath) // equals to: d => archPath(d)
+    .attr('stroke', '#fff')
+    .attr('stroke-width', 3);
+};
+
+// data array and firestore
+var data = [];
+
+db.collection('expenses').onSnapshot(res => {
+  res.docChanges().forEach(change => {
+    const doc = { ...change.doc.data(), id: change.doc.id };
+
+    switch (change.type) {
+      case 'added':
+        data.push(doc);
+        break;
+      case 'modified':
+        const index = data.findIndex(item => item.id === doc.id); // indexOf expects actual value while findIndex expects callback
+        data[index] = doc; // overwrite the original record
+        break;
+      case 'removed':
+        data = data.filter(item => item.id !== doc.id);
+        break;
+      default:
+        break;
+    }
+  });
+
+  update(data);
+});
