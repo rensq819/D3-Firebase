1d17068d8772e6d247fad91aa44dc2a7773568be
Siqi Ren <siqi.ren@gmail.com>
2019-11-17 22:54:20 -0500

    adding data to firestore

--- 9. App - Fitness Tracker/index.js
+++ 9. App - Fitness Tracker/index.js
@@ -26,3 +26,28 @@ btns.forEach(btn => {
     formAct.textContent = activity;
   });
 });
+
+// form submit
+form.addEventListener('submit', e => {
+  // prevent default action (reload)
+  e.preventDefault();
+
+  const distance = parseInt(input.value);
+  if (distance) {
+    db.collection('activities')
+      .add({
+        distance,
+        activity,
+        date: new Date().toString()
+      })
+      .then(() => {
+        error.textContent = '';
+        input.value = '';
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else {
+    error.textContent = 'Please enter a distance.';
+  }
+});
